e84715428d5991325b303f51ed6380c1fdb34853
upoudel888 <upoudel888@gmail.com>
2022-04-22 09:11:56 +0545

    threshold reduced and allowed time increased

---
+++ src/components/Description/Description.js
@@ -0,0 +1,12 @@
+import './Description.css'
+const Description = () => {
+    return (  
+        <div className="description">
+            <span className="big">2048 game</span><br />
+            Aim to merge two similar tiles to get a higher numbered tile. <br/>
+            Use <b>Arrow Keys</b> or <b> A,S,D,W</b> to move tiles. <br />
+        </div>
+    );
+}
+ 
+export default Description;
